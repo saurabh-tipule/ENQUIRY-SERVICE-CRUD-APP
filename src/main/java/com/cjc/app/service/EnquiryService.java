54ecf4d12ca9cbea2ea9fac80713f014e53d4e2d
unknown <priynkagore22@gmail.com>
2024-12-16 16:27:59 +0530

    get end point created

--- src/main/java/com/cjc/app/service/EnquiryService.java
+++ src/main/java/com/cjc/app/service/EnquiryService.java
@@ -1,5 +1,15 @@
 package com.cjc.app.service;
 
+import java.util.List;
+
+import com.cjc.app.model.Enquiry;
+
 public interface EnquiryService {
+
+	List<Enquiry> getAllEnquiry();
+
+	Enquiry getsingleEnquiry(Integer customerid);
+
+	
 
 }
--- src/main/java/com/cjc/app/serviceimpl/EnquiryServiceImpl.java
+++ src/main/java/com/cjc/app/serviceimpl/EnquiryServiceImpl.java
@@ -1,8 +1,13 @@
 package com.cjc.app.serviceimpl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigData.Option;
 import org.springframework.stereotype.Service;
 
+import com.cjc.app.model.Enquiry;
 import com.cjc.app.repository.EnquiryRepository;
 import com.cjc.app.service.EnquiryService;
 
@@ -14,6 +19,22 @@ public class EnquiryServiceImpl implements EnquiryService {
 	@Autowired
 	public void setEnquiryRepository(EnquiryRepository enquiryRepository) {
 		this.enquiryRepository = enquiryRepository;
+	}
+
+	@Override
+	public List<Enquiry> getAllEnquiry() {
+
+		return enquiryRepository.findAll();
+	}
+
+	@Override
+	public Enquiry getsingleEnquiry(Integer customerid) {
+
+		Optional<Enquiry> id = enquiryRepository.findById(customerid);
+		if (id.isPresent()) {
+			return id.get();
+		}
+		return null;
 	}
 
 }
